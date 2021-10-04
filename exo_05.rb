57a059d8e6e438ae874869eb19848842523efbf1
iPadrx <91556115+iPadrx@users.noreply.github.com>
2021-10-04 12:35:43 +0200

    Create exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,34 @@
+#affiche une chaîne de caractères
+puts "On va compter le nombre d'heures de travail à THP"
+#affiche une chaîne de caractère intégrant le résultat du calcul
+puts "Travail : #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#affiche une chaîne de caractères
+puts "Et en secondes ?"
+
+#affiche le résultat du calcul
+puts 10 * 5 * 11 * 60 * 60
+
+#affiche une chaîne de caractères sans effectuer le calcul ni la comparaison
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#effectue une comparaison (booleen)
+puts 3 + 2 < 5 - 7
+
+#affiche une chaîne de caractère intégrant le résultat du calcul
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#affiche une chaîne de caractère
+puts "Ok, c'est faux alors !"
+
+puts "C'est drôle ça, faisons-en plus :"
+
+#affiche une chaîne de caractère intégrant le résultat d'une comparaison 
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+
+
+# #{} sert à insérer une opération dans une chaîne de caractères, quelque soit l'opération 
